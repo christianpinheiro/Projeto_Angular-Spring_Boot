aea3c72deb4311e4a6df130e96348997ac78d16c
WilsonIglesias <wilsoniglesias2502@gmail.com>
2022-06-07 22:32:46 -0300

    feature/Tables_Component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,17 @@
+import { BudgetComponent } from './budget/budget.component';
+import { BrandComponent } from './brand/brand.component';
+import { PartComponent } from './part/part.component';
+import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UserComponent},
+  {path: 'parts', component: PartComponent},
+  {path: 'brands', component: BrandComponent},
+  {path: 'budgets', component: BudgetComponent}
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { UserComponent } from './user/user.component';
+import { PartComponent } from './part/part.component';
+import { BrandComponent } from './brand/brand.component';
+import { BudgetComponent } from './budget/budget.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    UserComponent,
+    PartComponent,
+    BrandComponent,
+    BudgetComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ],
   exports:[
     HomeComponent
